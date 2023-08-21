9d6a4ba6b84a518938d90ddcd4637ad69c6a3bda
taiki1frsh <taiki.furusho@gmail.com>
2023-08-22 00:36:54 +0900

    remove comment and add new

--- contracts/packet-forward/src/execute/claim_failed_request.rs
+++ contracts/packet-forward/src/execute/claim_failed_request.rs
@@ -4,7 +4,6 @@ use crate::state::FAILED_REQUESTS;
 use cosmwasm_std::{BankMsg, CosmosMsg};
 use cosmwasm_std::{DepsMut, Env, MessageInfo, Response};
 
-/// Only owner can execute it.
 #[cfg(not(feature = "library"))]
 pub fn execute_claim_failed_request(
     deps: DepsMut,
@@ -14,6 +13,7 @@ pub fn execute_claim_failed_request(
 ) -> Result<Response, ContractError> {
     let mut response = Response::new();
 
+    // This line returns error when the sender of the msg is not the correct emergency claimer.
     let request = FAILED_REQUESTS.load(deps.storage, (&info.sender, msg.request_id))?;
 
     FAILED_REQUESTS.remove(deps.storage, (&request.emergency_claimer, request.id));
